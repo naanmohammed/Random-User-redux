9a17f20a242bd0038eefaa3f57e22031ca9051ce
naanmohammed <naanahmed24@gmail.com>
2023-05-16 10:55:28 +0300

    Update app to fetch user list

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import UserList from './UserList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>User List</h1>
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
